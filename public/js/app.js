94d7b2411f8c099648cfda555e33eb49ac8d81a7
Joao Henrique Botelho <joao.botelho@ibm.com>
2022-11-11 15:12:27 -0300

    Setup Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,7 @@ weatherForm.addEventListener('submit', (event) => {
     const location = search.value
 
     // console.log(location)
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             // console.log(data.error)
